e7e1daba87b9cb6e48c2dc1a6e154bab5efd4c06
sadra-f <sadrafouladi@gmail.com>
2024-11-15 23:27:21 +0330

    equality check and neighbor distance added

--- Models/Point.py
+++ Models/Point.py
@@ -1,4 +1,4 @@
-from Models.ND import NeighborDistance as PD
+from Models.ND import NeighborDistance as ND
 
 class Point:
     def __init__(self, id):
@@ -8,12 +8,17 @@ class Point:
 
 
     def add_neighbor(self, point, dist):
-        self.neighbors.append(PD(self, point, dist))
+        self.neighbors.append(ND(self, point, dist))
     
 
     def neighbors_list(self):
         return [i.neighbor for i in self.neighbors]
     
+    def neighbor_dist(self, point):
+        for i in self.neighbors:
+            if i.neighbor == point:
+                return i.distance
+        raise ValueError("point is not in neighbors list")
 
     def neighbor_dist_tuple(self):
         return [(i.neighbor, i.dist) for i in self.neighbors]
@@ -23,3 +28,15 @@ class Point:
 
     def visit(self):
         self.visited = True
+
+    def __eq__(self, value: object) -> bool:
+        if isinstance(value, Point) :
+            res += 1 if self.id == value.id else 0
+            for i in self.neighbors:
+                if i not in value.neighbors:
+                    res -= 1
+                    break
+            return res == 2
+        return False
+
+
