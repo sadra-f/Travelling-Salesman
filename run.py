a6e1b2d1c929627585fad4ec789c130665b86dab
sadra-f <sadrafouladi@gmail.com>
2024-11-17 00:35:18 +0330

    runs an example and prints the result

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from Models.Point import Point
-from Algo.DFS import DFS
+from Algo.TSP import TravelingSalesman
 A = Point("A")
 B = Point("B")
 C = Point("C")
@@ -12,12 +12,12 @@ A.add_neighbor(E, 3.5)
 A.add_neighbor(D, 5)
 
 B.add_neighbor(A , 5)
-B.add_neighbor(E , 3.5)
+B.add_neighbor(E , 4)
 B.add_neighbor(C , 5)
-B.add_neighbor(F , 3)
+B.add_neighbor(F , 5)
 
 C.add_neighbor(B, 5)
-C.add_neighbor(E, 3.5)
+C.add_neighbor(E, 4)
 C.add_neighbor(D, 5)
 
 D.add_neighbor(A, 5)
@@ -25,14 +25,19 @@ D.add_neighbor(E, 3.5)
 D.add_neighbor(C, 5)
 
 E.add_neighbor(A, 3.5)
-E.add_neighbor(B, 3.5)
-E.add_neighbor(C, 3.5)
+E.add_neighbor(B, 4)
+E.add_neighbor(C, 4)
 E.add_neighbor(D, 3.5)
 
 F.add_neighbor(B, 5)
 
 pl = [A,B,C,D,E,F]
 
-dfs = DFS(pl)
-dfs.dfs(A)
-print()
+bfs = TravelingSalesman(pl)
+res = bfs.run()
+for val in res.keys():
+    print(f"{val} : ", end='')
+    if res[val] is not None:
+        print(f"{res[val]} -> len = {res[val].total_distance()}")
+    else:
+        print("No Hamiltonian Path Found")
