ca92e3535b820f0919803c18c60c454a31d0ecbc
sadra-f <sadrafouladi@gmail.com>
2024-11-16 23:37:47 +0330

    automated id generation and added neighbor_dist variable

--- Models/Point.py
+++ Models/Point.py
@@ -1,27 +1,29 @@
 from Models.ND import NeighborDistance as ND
 
 class Point:
-    def __init__(self, id):
-        self.id = id
+    id = 0
+    def __init__(self, str_rep=None):
+        self.str_rep = str_rep
+        self._id = Point.id
+        Point.id += 1
+        self.neighbors_distances = []
         self.neighbors = []
         self.visited = False
 
 
     def add_neighbor(self, point, dist):
-        self.neighbors.append(ND(self, point, dist))
+        self.neighbors_distances.append(ND(self, point, dist))
+        self.neighbors.append(point)
     
-
-    def neighbors_list(self):
-        return [i.neighbor for i in self.neighbors]
     
     def neighbor_dist(self, point):
-        for i in self.neighbors:
+        for i in self.neighbors_distances:
             if i.neighbor == point:
                 return i.distance
         raise ValueError("point is not in neighbors list")
 
     def neighbor_dist_tuple(self):
-        return [(i.neighbor, i.dist) for i in self.neighbors]
+        return [(i.neighbor, i.dist) for i in self.neighbors_distances]
     
     def reset_visit(self):
         self.visited = False
@@ -31,12 +33,11 @@ class Point:
 
     def __eq__(self, value: object) -> bool:
         if isinstance(value, Point) :
-            res += 1 if self.id == value.id else 0
-            for i in self.neighbors:
-                if i not in value.neighbors:
+            res = 0
+            res += 1 if self._id == value._id else 0
+            for i in self.neighbors_distances:
+                if i not in value.neighbors_distances:
                     res -= 1
                     break
             return res == 2
         return False
-
-
