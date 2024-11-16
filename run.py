bd3b1d8298fce9ba1d13529afc385b3a68a7a0f8
sadra-f <sadrafouladi@gmail.com>
2024-11-17 00:42:36 +0330

    changed the example

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ C = Point("C")
 D = Point("D")
 E = Point("E")
 F = Point("F")
+G = Point("G")
 
 A.add_neighbor(B, 5)
 A.add_neighbor(E, 3.5)
@@ -23,6 +24,7 @@ C.add_neighbor(D, 5)
 D.add_neighbor(A, 5)
 D.add_neighbor(E, 3.5)
 D.add_neighbor(C, 5)
+D.add_neighbor(G, 5)
 
 E.add_neighbor(A, 3.5)
 E.add_neighbor(B, 4)
@@ -31,7 +33,9 @@ E.add_neighbor(D, 3.5)
 
 F.add_neighbor(B, 5)
 
-pl = [A,B,C,D,E,F]
+G.add_neighbor(D, 5)
+
+pl = [A,B,C,D,E,F,G]
 
 bfs = TravelingSalesman(pl)
 res = bfs.run()
