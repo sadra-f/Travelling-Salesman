a2b95d3380e575953816ba351aaab0540b9c6594
sadra-f <sadrafouladi@gmail.com>
2024-11-17 00:28:06 +0330

    computes the hamiltonian path from each point and saves the best route

--- Algo/TSP.py
+++ Algo/TSP.py
@@ -4,15 +4,25 @@ from Models.Route import Route
 class TravelingSalesman:
     def __init__(self, points):
         self.points = points
-        self.routes = {p : [] for p in self.points}
-        self.best_routes = {p : Route(p) for p in self.points}
+        self.routes = {p : None for p in self.points}
+        self.best_route = {p : None for p in self.points}
 
     def run(self):
-        pass
+        for p in self.points:
+            routes = self.bfs(p)
+            if len(routes) > 0:
+                self.routes[p] = routes
+                best = routes[0].total_distance()
+                self.best_route[p] = routes[0]
+                for r in routes:
+                    if r.total_distance() < best:
+                        best = r.total_distance()
+                        self.best_route[p] = r
+        return self.best_route
+
 
 
     def bfs(self, start:Point):
-        #use BFS instead of DFS (use queue) make a new route for each neighbor newly visited keep the visited nodes in the route
         routes = []
         queue = [Route(start)]
 
