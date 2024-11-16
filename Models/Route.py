8020810d329cd02e511be4636865ce27a8fbfa0a
sadra-f <sadrafouladi@gmail.com>
2024-11-16 23:54:17 +0330

    added repr method and some debugging

--- Models/Route.py
+++ Models/Route.py
@@ -1,7 +1,7 @@
 from Models.PD import PointDistance as PD
 from Models.Point import Point
 class Route:
-    def __init__(self, start):
+    def __init__(self, start:Point):
         self.start = start
         self.steps = []
         self.points = [start]
@@ -9,12 +9,13 @@ class Route:
     def add_point(self, point:Point):
         if point not in self.points[-1].neighbors:
             raise ValueError("new point is not a neighbors of the last point")
-        self.steps.append(PD(self.start, point, self.points[-1].neighbor_dist(point)))
+        self.steps.append(self.points[-1].neighbor_dist(point))
         self.points.append(point)
 
-    def copy(self) -> Route:
+    def copy(self) -> 'Route':
         res = Route(self.start)
         res.steps = list(self.steps)
+        res.points = list(self.points)
         return res
 
     def visited_all(self, points) -> bool:
@@ -27,4 +28,7 @@ class Route:
         return point in self.points
 
     def final(self) -> Point:
-        return self.steps[-1]
+        return self.points[-1]
+    
+    def __repr__(self) -> str:
+        return f"{self.points}"
