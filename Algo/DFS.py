5779432dc2995df88f9c27250fad9d9ff16c551a
sadra-f <sadrafouladi@gmail.com>
2024-11-16 23:02:05 +0330

    added dfs to run on one node

---
+++ Algo/DFS.py
@@ -0,0 +1,32 @@
+from Models.Point import Point
+from Models.Route import Route
+
+class DFS:
+    def __init__(self, points):
+        self.points = points
+        self.routes = []
+
+    def run(self):
+        pass
+
+
+    def dfs(self, start:Point):
+        #use BFS instead of DFS (use queue) make a new route for each neighbor newly visited keep the visited nodes in the route
+        routes = []
+        queue = [Route(start)]
+
+        while len(queue) > 0:
+            current_r = queue.pop(0)
+            if current_r.visited_all(self.points):
+                routes.append(current_r)
+                continue
+            for p in current_r.final().neighbors:
+                if not current_r.visited(p):
+                    tmp_r = current_r.copy()
+                    tmp_r.add_point(p)
+                    queue.append(tmp_r)
+        
+        return routes
+
+
+
